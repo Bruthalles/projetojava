bdb65df99785746efb02beca5ba3a7c4a39e1e6a
Bruthalles <128196904+Bruthalles@users.noreply.github.com>
2023-11-13 12:52:25 -0300

    Add files via upload

---
+++ JavaByCake.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javabycake;
+
+/**
+ *
+ * @author 202303535287
+ */
+public class JavaByCake {
+    public static void main(String[] args) {
+        
+    }
+   
+}
